742418b8df05a39b45f42d7c563b9b509deb8743
Pavel Khorev <khorev05@list.ru>
2026-02-23 00:08:04 +0700

    feat: добавлены стартовые данные (тарифы, кредиты, операции погашения)

--- CreditsService/Migrations/20260222164643_SeedCredits.Designer.cs
+++ CreditsService/Migrations/20260222164643_SeedCredits.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CreditsService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CreditsService.Migrations
 {
     [DbContext(typeof(CreditsDbContext))]
-    partial class CreditsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260222164643_SeedCredits")]
+    partial class SeedCredits
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
