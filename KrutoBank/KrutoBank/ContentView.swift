3ef3d20a04764f39ac217f4ffc6445abff9c9c16
bdvzt <budaevazay@mail.ru>
2026-02-24 14:48:09 +0700

    Initial commit

---
+++ KrutoBank/KrutoBank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KrutoBank
+//
+//  Created by Zayata Budaeva on 24.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KrutoBank/KrutoBank/KrutoBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrutoBankApp.swift
+//  KrutoBank
+//
+//  Created by Zayata Budaeva on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct KrutoBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
