a8232aff4bfa945b38eaa219c859e4050cfed3e3
Kseniya Lipovskaya <lipovskayakseniya62@gmail.com>
2026-03-01 00:25:10 +0700

    Реализация формы входа

---
+++ frontend/src/app/features/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  LoginCredentials,
+  LoginFormComponent,
+} from '../../components/login-form/login-form.component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../core/auth/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [LoginFormComponent, CommonModule],
+  templateUrl: './login-page.component.html',
+  standalone: true,
+  styleUrl: './login-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginPageComponent {
+  private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
+
+  readonly pageLoading = signal(false);
+
+  async onLogin(credentials: LoginCredentials) {
+    this.pageLoading.set(true);
+
+    try {
+      await this.auth.login(credentials).toPromise();
+
+      await this.router.navigateByUrl('/');
+    } catch (e: any) {
+      console.error(e);
+    } finally {
+      this.pageLoading.set(false);
+    }
+  }
+}
