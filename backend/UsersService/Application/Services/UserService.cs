79d4ac809d6c5f3758d4ef618db807a96474338a
Pavel Khorev <khorev05@list.ru>
2026-03-25 01:32:34 +0700

    fghd sdgjjgkjdfn sdfgjkdfngkfld fjgklfdg

--- backend/UsersService/Application/Services/UserService.cs
+++ backend/UsersService/Application/Services/UserService.cs
@@ -6,7 +6,7 @@ using UsersService.Infrastructure.Auth;
 using UsersService.Infrastructure.Errors.Exceptions;
 using UsersService.Infrastructure.Mappers;
 using UsersService.Infrastructure.Repositories;
-using UsersService.Infrastructure.Helpers;
+using System.Linq;
 using ValidationException = UsersService.Infrastructure.Errors.Exceptions.ValidationException;
 
 namespace UsersService.Services;
@@ -166,5 +166,24 @@ public class UserService(
             throw new NotFoundException("Пользователь не найден");
 
         return user.ToDto();
+    }
+}
+
+public static class PhoneHelper
+{
+    public static string Normalize(string phone)
+    {
+        if (string.IsNullOrWhiteSpace(phone))
+            return phone;
+
+        var digits = new string(phone.Where(char.IsDigit).ToArray());
+
+        if (digits.StartsWith("8"))
+            digits = "7" + digits.Substring(1);
+
+        if (!digits.StartsWith("7"))
+            digits = "7" + digits;
+
+        return digits;
     }
 }
