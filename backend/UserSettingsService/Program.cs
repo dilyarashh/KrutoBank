347dee459681ee0ad44bc05a4a223a90e4a9774d
Pavel Khorev <khorev05@list.ru>
2026-03-24 23:38:36 +0700

    правка применения автомиграции

--- backend/UserSettingsService/Program.cs
+++ backend/UserSettingsService/Program.cs
@@ -65,4 +65,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UserSettingsDbContext>();
+    await db.Database.MigrateAsync();
+}
+
 app.Run();
